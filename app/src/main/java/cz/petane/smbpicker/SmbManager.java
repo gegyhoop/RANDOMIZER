0ae4985baad18631121a138d79aa5047d6fd697b
gegyhoop <gegy.hoop@seznam.cz>
2026-07-21 21:05:00 +0200

    Refactor SmbManager to use Profile instead of SettingsManager

--- app/src/main/java/cz/petane/smbpicker/SmbManager.java
+++ app/src/main/java/cz/petane/smbpicker/SmbManager.java
@@ -1,111 +1,39 @@
-package cz.petane.filmy;
+package cz.petane.smbpicker;
 
 import jcifs.CIFSContext;
 import jcifs.config.PropertyConfiguration;
 import jcifs.context.BaseContext;
 import jcifs.smb.NtlmPasswordAuthenticator;
 import jcifs.smb.SmbFile;
-
 import java.util.Properties;
 
 public class SmbManager {
 
-    private final SettingsManager settings;
+    private final Profile profile;   // změna z SettingsManager
 
-    public SmbManager(SettingsManager settings) {
-        this.settings = settings;
+    public SmbManager(Profile profile) {
+        this.profile = profile;
     }
-public CIFSContext getContext() throws Exception {
 
-    Properties props = new Properties();
+    public CIFSContext getContext() throws Exception {
+        Properties props = new Properties();
+        props.setProperty("jcifs.smb.client.enableSMB2", "true");
+        props.setProperty("jcifs.smb.client.disableSMB1", "false");
 
-    props.setProperty(
-            "jcifs.smb.client.enableSMB2",
-            "true"
-    );
+        CIFSContext base = new BaseContext(new PropertyConfiguration(props));
 
-    props.setProperty(
-            "jcifs.smb.client.disableSMB1",
-            "false"
-    );
-
-
-    CIFSContext base =
-            new BaseContext(
-                    new PropertyConfiguration(props)
+        if (!profile.isAnonymous()) {
+            base = base.withCredentials(
+                new NtlmPasswordAuthenticator(
+                    "",
+                    profile.getUsername(),
+                    profile.getPassword()
+                )
             );
-
-
-    if (!settings.isAnonymous()) {
-
-        base =
-                base.withCredentials(
-                        new NtlmPasswordAuthenticator(
-                                "",
-                                settings.getUsername(),
-                                settings.getPassword()
-                        )
-                );
+        }
+        return base;
     }
-
-
-    return base;
-}
 
     public boolean testConnection() {
-
         try {
-
-            Properties props = new Properties();
-
-            props.setProperty(
-                    "jcifs.smb.client.enableSMB2",
-                    "true"
-            );
-
-            props.setProperty(
-                    "jcifs.smb.client.disableSMB1",
-                    "false"
-            );
-
-
-            CIFSContext base =
-                    new BaseContext(
-                            new PropertyConfiguration(props)
-                    );
-
-
-            if (!settings.isAnonymous()) {
-
-                base = base.withCredentials(
-                        new NtlmPasswordAuthenticator(
-                                "",
-                                settings.getUsername(),
-                                settings.getPassword()
-                        )
-                );
-            }
-
-
-    String path =
-        "smb://" +
-        settings.getServer() +
-        "/" +
-        settings.getSource() +
-        "/";
-
-
-            SmbFile file = new SmbFile(path, base);
-
-
-            return file.exists();
-
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-            return false;
-        }
-    }
-}
+            String
