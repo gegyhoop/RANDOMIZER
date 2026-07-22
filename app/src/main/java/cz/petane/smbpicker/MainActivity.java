52f79bc701f3741a9ea5d53caa003e8fb68fe7b9
gegyhoop <gegy.hoop@seznam.cz>
2026-07-22 09:22:28 +0200

    Refactor MainActivity layout and UI components

--- app/src/main/java/cz/petane/smbpicker/MainActivity.java
+++ app/src/main/java/cz/petane/smbpicker/MainActivity.java
@@ -1,27 +1,53 @@
 package cz.petane.smbpicker;
 
+
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        TextView tv = new TextView(this);
-        tv.setText("Appka startovala\n\nPokud vidíš toto, dashboard selhal dříve.");
-        tv.setTextSize(18);
-        setContentView(tv);
 
-        try {
-            ProfileManager pm = new ProfileManager(this);
-            Toast.makeText(this, "ProfileManager OK - počet profilů: " + pm.getAllProfiles().size(), Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Toast.makeText(this, "CHYBA: " + e.getClass().getSimpleName() + "\n" + e.getMessage(), Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
+        TextView text = new TextView(this);
+
+        text.setText(
+                "SMB Random Picker\n\n"
+                + "Aplikace spuštěna"
+        );
+
+        text.setTextSize(22);
+
+
+        Button button = new Button(this);
+
+        button.setText("Test");
+
+
+        android.widget.LinearLayout layout =
+                new android.widget.LinearLayout(this);
+
+
+        layout.setOrientation(
+                android.widget.LinearLayout.VERTICAL
+        );
+
+
+        layout.addView(text);
+
+        layout.addView(button);
+
+
+        setContentView(layout);
+
     }
+
 }
