6937cb860dc0c2d49e439c3c0033d7b16c4d8b56
gegyhoop <gegy.hoop@seznam.cz>
2026-08-28 08:51:12 +0200

    Refactor SmbManager for improved connection handling

--- app/src/main/java/cz/petane/smbpicker/SmbManager.java
+++ app/src/main/java/cz/petane/smbpicker/SmbManager.java
@@ -1,85 +1,306 @@
 package cz.petane.smbpicker;
 
+
 import jcifs.CIFSContext;
-import jcifs.context.SingletonContext;
+import jcifs.config.PropertyConfiguration;
+import jcifs.context.BaseContext;
 import jcifs.smb.NtlmPasswordAuthenticator;
 import jcifs.smb.SmbFile;
 
+import java.util.Properties;
+
+
+
 public class SmbManager {
+
+
+    private final Profile profile;
 
     private final CIFSContext context;
 
-    public SmbManager(Profile profile) {
-        if(profile.isAnonymous()) {
-            context = SingletonContext.getInstance();
-        } else {
-            context = SingletonContext.getInstance()
-                    .withCredentials(
-                            new NtlmPasswordAuthenticator(
-                                    profile.getUsername(),
-                                    profile.getPassword()
-                            )
+
+
+    public SmbManager(Profile profile){
+
+
+        this.profile = profile;
+
+
+        try {
+
+
+            Properties props =
+                    new Properties();
+
+
+            props.setProperty(
+                    "jcifs.smb.client.minVersion",
+                    "SMB202"
+            );
+
+
+            props.setProperty(
+                    "jcifs.smb.client.maxVersion",
+                    "SMB210"
+            );
+
+
+            CIFSContext base =
+                    new BaseContext(
+                            new PropertyConfiguration(props)
                     );
+
+
+            if(!profile.isAnonymous()){
+
+                base =
+                        base.withCredentials(
+                                new NtlmPasswordAuthenticator(
+                                        "",
+                                        profile.getUsername(),
+                                        profile.getPassword()
+                                )
+                        );
+
+            }
+
+
+            context = base;
+
+
         }
+        catch(Exception e){
+
+            throw new RuntimeException(e);
+
+        }
+
+
     }
 
-    public SmbFile[] listFolder(String path) throws Exception {
-        return new SmbFile(path, context).listFiles();
+
+
+
+    public boolean testConnection(){
+
+
+        try {
+
+
+            SmbFile file =
+                    new SmbFile(
+                            getPath(profile.getSource()),
+                            context
+                    );
+
+
+            return file.exists();
+
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+            return false;
+
+
+        }
+
+
     }
+
+
+
+
+
+
+    public SmbFile[] listFolder(String folder)
+            throws Exception {
+
+
+        SmbFile dir =
+                new SmbFile(
+                        getPath(folder),
+                        context
+                );
+
+
+        return dir.listFiles();
+
+
+    }
+
+
+
+
+
+
+
+
 
     public boolean moveFile(
-            String source,
-            String target,
-            String fileName
-    ) throws Exception {
+            String fromFolder,
+            String toFolder,
+            String filename
+    ){
 
-        SmbFile from = new SmbFile(
-                source + "/" + fileName,
-                context
-        );
 
-        SmbFile to = new SmbFile(
-                target + "/" + fileName,
-                context
-        );
+        try{
 
-        from.renameTo(to);
-        return true;
+
+            SmbFile from =
+                    new SmbFile(
+                            getPath(fromFolder)
+                                    + filename,
+                            context
+                    );
+
+
+
+            SmbFile to =
+                    new SmbFile(
+                            getPath(toFolder)
+                                    + filename,
+                            context
+                    );
+
+
+
+            from.renameTo(to);
+
+
+
+            return true;
+
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+            return false;
+
+
+        }
+
+
     }
+
+
+
+
+
+
+
+
 
     public int moveAll(
-            String source,
-            String target
-    ) throws Exception {
+            String fromFolder,
+            String toFolder
+    ){
 
-        int count = 0;
 
-        SmbFile[] files = listFolder(source);
+        int moved = 0;
 
-        if(files == null) return 0;
 
-        for(SmbFile file : files) {
-            if(file.isFile()) {
-                moveFile(
-                        source,
-                        target,
+        try{
+
+
+            SmbFile folder =
+                    new SmbFile(
+                            getPath(fromFolder),
+                            context
+                    );
+
+
+
+            SmbFile[] files =
+                    folder.listFiles();
+
+
+
+
+            if(files == null)
+                return 0;
+
+
+
+
+
+            for(SmbFile file : files){
+
+
+                if(!file.isFile())
+                    continue;
+
+
+
+                if(moveFile(
+                        fromFolder,
+                        toFolder,
                         file.getName()
-                );
-                count++;
+                )){
+
+
+                    moved++;
+
+
+                }
+
+
             }
+
+
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+
         }
 
-        return count;
+
+
+        return moved;
+
+
     }
 
-    public boolean testConnection() {
-        try {
-            return new SmbFile(
-                    "smb://",
-                    context
-            ).exists();
-        } catch(Exception e) {
-            return false;
+
+
+
+
+
+
+
+
+
+
+
+    private String getPath(String folder){
+
+
+        while(folder.startsWith("/")){
+
+
+            folder =
+                    folder.substring(1);
+
+
         }
+
+
+
+        return "smb://"
+                + profile.getServer()
+                + "/"
+                + folder
+                + "/";
+
+
     }
+
+
 }
